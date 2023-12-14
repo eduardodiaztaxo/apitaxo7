63afadc2ccdf04c65e88470749f280c5fa0f3251
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2023-12-14 11:35:01 -0300

    remove other chars specials before db

--- app/Http/Controllers/Api/V1/RespActivoController.php
+++ app/Http/Controllers/Api/V1/RespActivoController.php
@@ -132,7 +132,7 @@ class RespActivoController extends Controller
                     'centro_costo'      => isset($item->centro_costo) ? $item->centro_costo : null,  
                     'localizacion'      => isset($item->localizacion) ? $item->localizacion : null,  
                     'fecha_compra'      => isset($item->fecha_compra) ? $item->fecha_compra : null, 
-                    'descripcion'       => isset($item->descripcion) ? $item->descripcion : null,  
+                    'descripcion'       => isset($item->descripcion) ? iconv('UTF-8','ASCII//TRANSLIT',$item->descripcion) : null,  
                     'etiqueta'          => isset($item->etiqueta) ? $item->etiqueta : null,  
                     'serie'             => isset($item->serie) ? $item->serie : null,   
                     'marca'             => isset($item->marca) ? $item->marca : null,   
