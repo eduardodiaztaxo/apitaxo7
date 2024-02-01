ab8d15b47da6d74390c0d0ab3ae58888db5ead41
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2024-02-01 10:06:53 -0300

    fix name env var db exmax

--- config/database.php
+++ config/database.php
@@ -110,7 +110,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE_ESMAX', 'forge'),
+            'database' => env('DB_DATABASE_ESMAX_CERT', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
