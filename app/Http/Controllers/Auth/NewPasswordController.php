b35f1da236c873d1bb563f3a96af4b3d708f7889
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2024-08-23 16:33:32 -0400

    forgot pass mail and api, old users reset pass as well

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Base\OldUser;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -54,12 +55,23 @@ class NewPasswordController extends Controller
             }
         );
 
+        if ($status == Password::PASSWORD_RESET) {
+
+            $oldUser = OldUser::where('user_login', '=', $request->email)->first();
+
+            if ($oldUser) {
+                $oldUser->user_pw = $request->password;
+                $oldUser->timestamps = false;
+                $oldUser->save();
+            }
+        }
+
         // If the password was successfully reset, we will redirect the user back to
         // the application's home authenticated view. If there is an error we can
         // redirect them back to where they came from with their error message.
         return $status == Password::PASSWORD_RESET
-                    ? redirect()->route('login')->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+            ? redirect()->route('login')->with('status', __($status))
+            : back()->withInput($request->only('email'))
+            ->withErrors(['email' => __($status)]);
     }
 }
