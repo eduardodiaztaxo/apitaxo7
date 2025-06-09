99937b17454666d188e0d73e09d6a7aa69cafb4f
Sol Briceño <65969018+Sol-Ninoska1@users.noreply.github.com>
2025-06-09 17:19:58 -0400

    picture_appsV10

--- app/Http/Controllers/Api/V1/CrudActivoController.php
+++ app/Http/Controllers/Api/V1/CrudActivoController.php
@@ -234,7 +234,7 @@ class CrudActivoController extends Controller
         
         $path = $this->imageService->optimizeImageAndSave(
             $request->file('imagen'),
-            "_lib/file/img/" . $request->user()->nombre_cliente . "/img",
+            "_lib/file/img/" . $request->user()->nombre_cliente . "/img"
         );
 
 
