9609f6a05d1682396a85b780e5dd9d314e73fa57
Sol Briceño <65969018+Sol-Ninoska1@users.noreply.github.com>
2025-07-03 13:36:39 -0400

    fix

--- routes/api.php
+++ routes/api.php
@@ -141,6 +141,7 @@ Route::middleware(['auth:sanctum', 'switch.database'])->prefix('v1')->group(func
     Route::get('marcas-disponibles/{etiqueta}', [CrudActivoController::class, 'marcasDisponibles']);
 
     Route::get('ciclos', [CiclosController::class, 'index']);
+    
 
     Route::get('ciclos/{ciclo}', [CiclosController::class, 'show']);
 
