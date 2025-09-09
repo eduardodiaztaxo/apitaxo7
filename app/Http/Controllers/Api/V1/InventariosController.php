700708794263ab96f56afb63de63a8a79a6a995e
Sol Briceño <65969018+Sol-Ninoska1@users.noreply.github.com>
2025-09-09 12:01:17 -0300

    fix

--- app/Http/Controllers/Api/V1/InventariosController.php
+++ app/Http/Controllers/Api/V1/InventariosController.php
@@ -159,7 +159,8 @@ class InventariosController extends Controller
         $inventario->serie               = $request->serie ?? '';
         $inventario->latitud             = $request->latitud ?? 0;
         $inventario->longitud            = $request->longitud ?? 0;
-        $inventario->precision           = $request->precision ?? 0;
+        $inventario->precision_geo       = $request->precision ?? 0;
+        $inventario->calidad_geo         = $request->calidad ?? 0;
         $inventario->capacidad           = $request->capacidad ?? '';
         $inventario->estado              = intval($request->estado ?? 0);
         $inventario->color               = intval($request->color ?? 0);
@@ -238,7 +239,8 @@ class InventariosController extends Controller
             'idMaterial'          => intval($request->idMaterial ?? null),
             'latitud'             => $request->latitud ?? null,
             'longitud'            => $request->longitud ?? null,
-            'precision'           => $request->precision ?? null,
+            'precision_geo'       => $request->precision ?? null,
+            'calidad_geo'         => $request->calidad ?? null,
             'capacidad'           => $request->capacidad,
             'estado'              => intval($request->estado ?? null),
             'color'               => intval($request->color ?? null),
