45f285a05da4474ce3c1f0d0c01840f4bf9202cd
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2025-11-24 16:16:16 -0300

    more customs fields attr 2

--- app/Services/InvConfigService.php
+++ app/Services/InvConfigService.php
@@ -33,6 +33,16 @@ class InvConfigService
             WHEN id_atributo = 46 THEN 'texto_abierto_18'
             WHEN id_atributo = 47 THEN 'texto_abierto_19'
             WHEN id_atributo = 48 THEN 'texto_abierto_20'
+            WHEN id_atributo = 49 THEN 'texto_abierto_21'
+            WHEN id_atributo = 50 THEN 'texto_abierto_22'
+            WHEN id_atributo = 51 THEN 'texto_abierto_23'
+            WHEN id_atributo = 52 THEN 'texto_abierto_24'
+            WHEN id_atributo = 53 THEN 'texto_abierto_25'
+            WHEN id_atributo = 54 THEN 'texto_abierto_26'
+            WHEN id_atributo = 55 THEN 'texto_abierto_27'
+            WHEN id_atributo = 56 THEN 'texto_abierto_28'
+            WHEN id_atributo = 57 THEN 'texto_abierto_29'
+            WHEN id_atributo = 58 THEN 'texto_abierto_30'
             ELSE ''
         END AS input_name,	
         type_input AS `type`,	
@@ -48,7 +58,7 @@ class InvConfigService
         valor_minimo AS `min`,
         valor_maximo AS `max`,
         label_input AS label
-        FROM inv_atributos WHERE id_atributo IN (27,28,29,30,31,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48) AND id_validacion != 0 AND id_grupo = ? AND id_proyecto = ?";
+        FROM inv_atributos WHERE id_atributo IN (27,28,29,30,31,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58) AND id_validacion != 0 AND id_grupo = ? AND id_proyecto = ?";
 
         $inputs = DB::select($sql, [$id_grupo, $id_proyecto]);
 
