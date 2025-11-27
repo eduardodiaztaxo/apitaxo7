648ded4b88e07a0a3308a38df392005567280192
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2025-11-27 16:34:08 -0300

    land markers service

---
+++ app/Http/Controllers/Api/V1/Maps/MapLandMarkerController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Maps;
+
+use App\Http\Controllers\Controller;
+use App\Models\Maps\MapLandMarker;
+use Illuminate\Http\Request;
+
+class MapLandMarkerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $markers = MapLandMarker::all();
+
+        return response()->json(
+            $markers,
+            200
+        );
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
