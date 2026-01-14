8cad0602035ce0f1b58b577ffda50bf10afd5e67
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2026-01-14 09:30:40 -0300

    update date of adjust coordinates in adjust_at field

--- app/Http/Controllers/Api/V1/InventariosController.php
+++ app/Http/Controllers/Api/V1/InventariosController.php
@@ -451,6 +451,8 @@ class InventariosController extends Controller
         $invObj->adjusted_lat = $request->adjusted_lat;
         $invObj->adjusted_lng = $request->adjusted_lng;
 
+        $invObj->adjusted_at = date('Y-m-d H:i:s');
+
         $invObj->save();
 
         return response()->json([
