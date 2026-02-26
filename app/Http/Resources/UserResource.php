d18c1372c037184bada46ab78d2f44d2e7c01fa4
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2026-02-26 11:08:08 -0300

    api user service

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class UserResource extends JsonResource
 {
@@ -24,6 +25,60 @@ class UserResource extends JsonResource
             $signature_register = 1;
         }
 
+
+                   
+        $role = $this->role;
+
+        if (!$role) {
+            return response()->json([
+                'message' => 'User does not have an associated role'
+            ], 400);
+        }
+
+            $permissions = DB::connection('mysql_auth')->table('entities')
+                ->leftJoin('roles_entities_permissions', 'entities.id', '=', 'roles_entities_permissions.entity_id')
+                ->where(function ($query) use ($role) {
+                    $query->where('roles_entities_permissions.role_id', $role->id)
+                        ->orWhereNull('roles_entities_permissions.role_id');
+                })
+                ->select(
+                    'entities.id',
+                    'entities.tag as entity_tag',
+                    'roles_entities_permissions.show',
+                    'roles_entities_permissions.edit',
+                    'roles_entities_permissions.create',
+                    'roles_entities_permissions.delete'
+                )
+                ->get();
+
+            $entities = DB::connection('mysql_auth')->table('entities')->pluck('tag')->toArray();
+            $formattedPermissions = [];
+
+            foreach ($entities as $entity) {
+                $permission = $permissions->firstWhere('entity_tag', $entity);
+
+                if ($permission) {
+                    // Si el ID de la entidad es 4, asignar todos los permisos 
+                    if ($permission->id == 4) {
+                        $formattedPermissions = [
+                            'zona' => ['show' => 1, 'edit' => 1, 'create' => 1, 'delete' => 1],
+                            'emplazamiento' => ['show' => 1, 'edit' => 1, 'create' => 1, 'delete' => 1],
+                            'bienes_marcas' => ['show' => 1, 'edit' => 1, 'create' => 1, 'delete' => 1],
+                            'mover_bienes' => ['show' => 1, 'edit' => 1, 'create' => 1, 'delete' => 1],
+                        ];
+                        break;
+                    } else {
+                        $formattedPermissions[$entity] = [
+                            'show' => $permission->show ?? 0,
+                            'edit' => $permission->edit ?? 0,
+                            'create' => $permission->create ?? 0,
+                            'delete' => $permission->delete ?? 0,
+                        ];
+                    }
+                }
+            }
+
+
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -37,7 +92,8 @@ class UserResource extends JsonResource
             "role" => $this->role ? $this->role->name : '',
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
-            'signature_register' => $signature_register
+            'signature_register' => $signature_register,
+            'permissions' => $formattedPermissions,
         ];
     }
 }
--- routes/api/v1/protected.php
+++ routes/api/v1/protected.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\V1\PostController as PostV1;
 use App\Http\Controllers\Api\V1\BajaDocumentoController as BajaV1;
 use App\Http\Controllers\Api\V1\Comunes\DatosActivosController;
 use App\Http\Controllers\Api\V1\RespLocalizacionController as RespLocV1;
+use App\Http\Controllers\Api\V1\User\UserController;
 
 // Importar rutas por entidad
 require __DIR__ . '/activos.php';
@@ -77,5 +78,7 @@ Route::middleware(['auth:sanctum', 'switch.database'])->prefix('v1')->group(func
     Route::post('create-bienes', [DatosActivosController::class, 'createBienes']);
 
     Route::post('create-marcas', [DatosActivosController::class, 'createMarcas']);
+
+    Route::get('get-user', [UserController::class, 'show']);
 
 });
