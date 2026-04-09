b9c055c2d3032670fc7a3d528c34d8732e76e73b
Eduardo Díaz <edu.alejandrodiaz@gmail.com>
2026-04-09 15:34:28 -0400

    fix override id_inventario when updating in processes online multiple sync

--- app/Http/Controllers/Api/V1/InventariosController.php
+++ app/Http/Controllers/Api/V1/InventariosController.php
@@ -1182,7 +1182,7 @@ class InventariosController extends Controller
         $ultimo_id_inventario = DB::table('inv_inventario')
             ->where('id_proyecto', $id_proyecto)
             ->max('id_inventario');
-
+        //aqui falta un next val
         $siguiente_id_inventario = ($ultimo_id_inventario ?? 0) + 1;
 
         foreach ($items as $key => $item) {
@@ -1764,6 +1764,7 @@ class InventariosController extends Controller
                     $activo['id_img'] = $existsInv->id_img;
                     $idsi[$activo['etiqueta']] = $existsInv->id_img;
                 }
+                $activo['id_inventario'] = $existsInv->id_inventario;
                 $existsInv->update($activo);
                 $saved[] = $activo['etiqueta'];
             } else {
